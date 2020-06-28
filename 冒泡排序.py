e88007c3c91652fa8b12f589474bfa5073bbbed4
warmer <346488511@qq.com>
2020-06-28 18:12:02 +0800

    first

---
+++ 冒泡排序.py
@@ -0,0 +1,13 @@
+
+list1 = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+#list1 = [10, 55, 336, 54, 99, 65, 48]
+
+#总循环次数
+for i in range(len(list1)-1):
+    #每两个相邻位置对比大小
+    for j in range(len(list1)-i-1):
+        # 如果后一位比前一位小就交换位置
+        if list1[j]>list1[j+1]:
+            list1[j],list1[j+1] = list1[j+1],list1[j]
+            print(list1)
+print(list1)
